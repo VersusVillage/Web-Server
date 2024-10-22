26032c585464fc611477c8f3933ba0a306f1e057
jaenam615 <jaenam2060@gmail.com>
2024-10-22 20:36:42 +0900

    initial commit

---
+++ src/main/java/vv/versusvillage/VersusvillageApplication.java
@@ -0,0 +1,13 @@
+package vv.versusvillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersusvillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersusvillageApplication.class, args);
+	}
+
+}
